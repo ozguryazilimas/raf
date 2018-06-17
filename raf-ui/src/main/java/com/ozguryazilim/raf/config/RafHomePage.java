0a73e84fc860872ed4e6ac630caac8e98c425f07
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2018-06-17 20:21:16 +0300

    refs #29183 Ana sayfa boş kalmasın bir parça düzenlendi

---
+++ raf-ui/src/main/java/com/ozguryazilim/raf/config/RafHomePage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ozguryazilim.raf.config;
+
+import com.ozguryazilim.telve.auth.SecuredPage;
+import com.ozguryazilim.telve.view.Pages;
+import javax.enterprise.context.ApplicationScoped;
+import org.apache.deltaspike.jsf.api.config.view.View;
+
+/**
+ *
+ * @author oyas
+ */
+@ApplicationScoped
+@View(name = "rafHome") @SecuredPage
+public class RafHomePage implements Pages{
+    
+}
