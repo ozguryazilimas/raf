c86bb02f1c74e34a5ffe9fbea900a7660ff499f9
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2018-06-22 12:47:10 +0300

    refs #29017 mimeType iconları için svg sprite yapısı

--- raf-ui/src/main/java/com/ozguryazilim/raf/IconResolver.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/IconResolver.java
@@ -6,6 +6,8 @@
 package com.ozguryazilim.raf;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 import org.slf4j.Logger;
@@ -24,17 +26,49 @@ public class IconResolver implements Serializable{
    
     private static final Logger LOG = LoggerFactory.getLogger(IconResolver.class);
     
+    private static final List<String> knowMimeTypes = Arrays.asList(
+            "image-png", "image-jpeg", "image-bmp", "image-svg-xml", "image-gif", 
+            "application-pdf", "text-plain", "text-x-asciidoc", "text-x-web-markdown",
+            "text-csv", "application-x-sh", "text-html", "application-xml", "text-x-log",
+            "application-x-mobipocket-ebook", "application-epub-zip", 
+            "video-webm", "video-x-matroska", "video-x-mp4",
+            "audio-mp3", "audio-mpeg", 
+            "application-zip", "application-gzip", "application-x-bittorrent",
+            "application-vnd-openxmlformats-officedocument-spreadsheetml-sheet",
+            "application-vnd-openxmlformats-officedocument-presentationml-presentation",
+            "application-vnd-openxmlformats-officedocument-wordprocessingml-document",
+            "application-vnd-oasis-opendocument-spreadsheet",
+            "application-vnd-oasis-opendocument-presentation",
+            "application-vnd-oasis-opendocument-text",
+            "application-vnd-ms-excel",
+            "application-vnd-ms-powerpoint",
+            "application-msword"
+            );
+    
     public String getIcon( String mimeType ){
         LOG.debug("Icon ask for MimeType : {}", mimeType);
         switch (mimeType) {
             case "raf/folder":
-                return "fa-folder";
+                return "folder";
             case "raf/categories":
                 return "fa-sitemap";
             case "raf/tags":
                 return "fa-tags";
             case "raf/search":
                 return "fa-search";
+
+            default:
+                String s = mimeType.replace("/", "-").replace(".", "-").replace("+", "-");
+                if( knowMimeTypes.contains(s)){
+                    return s;
+                } else {
+                    //FIXME: burda aslında ana gruplara bakılabilir text-*, video* v.b. en son olarak da Unknown
+                    return "text-plain";
+                }
+                
+                
+                
+                /*
                 
             case "application/pdf":
                 return "fa-file-pdf-o";
@@ -73,6 +107,7 @@ public class IconResolver implements Serializable{
                 
             default:
                 return "fa-file-o";
+            */
         }
     }
     
