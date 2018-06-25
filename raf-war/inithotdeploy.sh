31832319a6fca0b89beb69e2249a10fb8af73952
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2018-06-25 18:16:53 +0300

    refs #29318 bpmn viewer yapıldı

--- raf-war/inithotdeploy.sh
+++ raf-war/inithotdeploy.sh
@@ -16,6 +16,7 @@ rm -rf WEB-INF/lib/raf-core-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-ui-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-service-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-pdf-1.0.0-SNAPSHOT.jar
+rm -rf WEB-INF/lib/raf-bpmn-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-invoice-1.0.0-SNAPSHOT.jar
 
 
@@ -33,6 +34,7 @@ ln -s ~/git/raf/raf-core/target/classes/ WEB-INF/lib/raf-core-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-ui/target/classes/ WEB-INF/lib/raf-ui-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-service/target/classes/ WEB-INF/lib/raf-service-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-pdf/target/classes/ WEB-INF/lib/raf-pdf-1.0.0-SNAPSHOT.jar
+ln -s ~/git/raf/raf-bpmn/target/classes/ WEB-INF/lib/raf-bpmn-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-invoice/target/classes/ WEB-INF/lib/raf-invoice-1.0.0-SNAPSHOT.jar
 
 
