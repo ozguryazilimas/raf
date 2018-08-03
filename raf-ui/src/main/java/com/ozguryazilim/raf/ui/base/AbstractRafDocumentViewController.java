797a568579377a2170d672d013396a1f47d77f84
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2018-08-03 10:25:09 +0300

    refs #29157 Config'den version takibi kapatılabiliyor artık
    ( Şu anda sadece UI hilesi )

--- raf-ui/src/main/java/com/ozguryazilim/raf/ui/base/AbstractRafDocumentViewController.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/ui/base/AbstractRafDocumentViewController.java
@@ -13,72 +13,91 @@ import com.ozguryazilim.raf.models.RafVersion;
 import com.ozguryazilim.telve.messages.FacesMessages;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import org.apache.deltaspike.core.api.config.ConfigResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * RafDocument view controlü için taban sınıf.
- * 
+ *
  * Üzerinde View Id, PreviewPanel ile ilgili kontroller bulunuyor.
- * 
+ *
  * @author Hakan Uygun
  */
-public class AbstractRafDocumentViewController extends AbstractRafObjectViewController<RafDocument>{
+public class AbstractRafDocumentViewController extends AbstractRafObjectViewController<RafDocument> {
 
     private static final Logger LOG = LoggerFactory.getLogger(AbstractRafDocumentViewController.class);
-    
+
     @Inject
     private FileUploadAction fileUploadAction;
-    
-    @Inject 
+
+    @Inject
     private RafService rafService;
-    
+
     private List<RafVersion> versions = null;
+
+    private Boolean versionManagementEnabled;
+
+    @PostConstruct
+    public void init() {
+        versionManagementEnabled = "true".equals(ConfigResolver.getPropertyValue("raf.version.enabled", "false"));
+    }
 
     @Override
     public void setObject(RafDocument object) {
-        super.setObject(object); 
+        super.setObject(object);
         versions = null;
     }
-    
+
     @Override
     public String getViewId() {
         return "/fragments/documentView.xhtml";
     }
-    
+
     /**
-     * Geriye mimeType'a göre hangi widget kullanılacak ise onun fragman bilgisini döner.
-     * 
-     * @return 
+     * Geriye mimeType'a göre hangi widget kullanılacak ise onun fragman
+     * bilgisini döner.
+     *
+     * @return
      */
-    public String getPreviewWidget(){
+    public String getPreviewWidget() {
         //Eğer mimetype yoksa default isteyelim
-        if( getObject() != null ){
+        if (getObject() != null) {
             return PreviewPanelRegistery.getMimeTypePanel(getObject().getMimeType()).getViewId();
         } else {
             return PreviewPanelRegistery.getMimeTypePanel("default").getViewId();
         }
-        
+
     }
-    
-    public void checkin(){
+
+    public void checkin() {
         fileUploadAction.execute("CHECKIN", getObject().getPath());
     }
-    
-    public List<RafVersion> getVersionHistory(){
-        
-        if( versions == null ){
-            try {
-                versions = rafService.getVersionHistory(getObject());
-            } catch (RafException ex) {
-                LOG.error("Raf Exception", ex);
-                FacesMessages.error(ex.getMessage());
+
+    public List<RafVersion> getVersionHistory() {
+
+        if (versions == null) {
+            if (getVersionManagementEnabled()) {
+                try {
+                    versions = rafService.getVersionHistory(getObject());
+                } catch (RafException ex) {
+                    LOG.error("Raf Exception", ex);
+                    FacesMessages.error(ex.getMessage());
+                    versions = new ArrayList<>();
+                }
+            } else {
                 versions = new ArrayList<>();
             }
         }
-        
+
         return versions;
     }
+
+    public Boolean getVersionManagementEnabled() {
+        return versionManagementEnabled;
+    }
+
     
 }
