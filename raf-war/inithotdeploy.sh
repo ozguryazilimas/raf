12852481d5c751f0c015f137086e0f18f0024d93
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2019-03-31 18:26:25 +0300

    refs #32824 raf-record-war projesi ve biraz branding

--- raf-war/inithotdeploy.sh
+++ raf-war/inithotdeploy.sh
@@ -8,9 +8,9 @@ rm -rf WEB-INF/lib/raf-service-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-pdf-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-bpmn-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-invoice-1.0.0-SNAPSHOT.jar
-rm -rf WEB-INF/lib/raf-jbpm-1.0.0-SNAPSHOT.jar
+#rm -rf WEB-INF/lib/raf-jbpm-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-forms-1.0.0-SNAPSHOT.jar
-rm -rf WEB-INF/lib/raf-record-1.0.0-SNAPSHOT.jar
+#rm -rf WEB-INF/lib/raf-record-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-converter-1.0.0-SNAPSHOT.jar
 rm -rf WEB-INF/lib/raf-webdav-1.0.0-SNAPSHOT.jar
 # rm -rf WEB-INF/lib/raf-cmis-1.0.0-SNAPSHOT.jar
@@ -22,9 +22,9 @@ ln -s ~/git/raf/raf-service/target/classes/ WEB-INF/lib/raf-service-1.0.0-SNAPSH
 ln -s ~/git/raf/raf-pdf/target/classes/ WEB-INF/lib/raf-pdf-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-bpmn/target/classes/ WEB-INF/lib/raf-bpmn-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-invoice/target/classes/ WEB-INF/lib/raf-invoice-1.0.0-SNAPSHOT.jar
-ln -s ~/git/raf/raf-jbpm/target/classes/ WEB-INF/lib/raf-jbpm-1.0.0-SNAPSHOT.jar
+#ln -s ~/git/raf/raf-jbpm/target/classes/ WEB-INF/lib/raf-jbpm-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-forms/target/classes/ WEB-INF/lib/raf-forms-1.0.0-SNAPSHOT.jar
-ln -s ~/git/raf/raf-record/target/classes/ WEB-INF/lib/raf-record-1.0.0-SNAPSHOT.jar
+#ln -s ~/git/raf/raf-record/target/classes/ WEB-INF/lib/raf-record-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-converter/target/classes/ WEB-INF/lib/raf-converter-1.0.0-SNAPSHOT.jar
 ln -s ~/git/raf/raf-webdav/target/classes/ WEB-INF/lib/raf-webdav-1.0.0-SNAPSHOT.jar
 # ln -s ~/git/raf/raf-cmis/target/classes/ WEB-INF/lib/raf-cmis-1.0.0-SNAPSHOT.jar
