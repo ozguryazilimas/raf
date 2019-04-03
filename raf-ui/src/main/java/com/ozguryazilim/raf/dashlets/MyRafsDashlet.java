430eb16a78905d0d2970ae35b3f4daf50b22c69e
Serdar Mutlu <serdar.mutlu@ozguryazilim.com.tr>
2019-04-03 16:13:38 +0300

    refs #31823 update rafs dashlet if added or changed or deleted raf

--- raf-ui/src/main/java/com/ozguryazilim/raf/dashlets/MyRafsDashlet.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/dashlets/MyRafsDashlet.java
@@ -2,12 +2,13 @@ package com.ozguryazilim.raf.dashlets;
 
 import com.ozguryazilim.raf.definition.RafDefinitionService;
 import com.ozguryazilim.raf.entities.RafDefinition;
+import com.ozguryazilim.raf.events.RafDataChangedEvent;
 import com.ozguryazilim.telve.auth.Identity;
 import com.ozguryazilim.telve.dashboard.AbstractDashlet;
 import com.ozguryazilim.telve.dashboard.Dashlet;
 import com.ozguryazilim.telve.dashboard.DashletCapability;
-import com.ozguryazilim.telve.nav.NagivationController;
 import java.util.List;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 /**
@@ -30,9 +31,17 @@ public class MyRafsDashlet extends AbstractDashlet{
         super.load(); //To change body of generated methods, choose Tools | Templates.
         rafs = rafDefinitionService.getRafsForUser(identity.getLoginName(), Boolean.TRUE);
     }
-
+    
     public List<RafDefinition> getRafs() {
         return rafs;
+    }
+    
+    /**
+     * Yeni raf eklenir,silinir ya da ismi değişirse dashlet'i güncelle
+     * @param event 
+     */
+    public void rafDataChangedListener(@Observes RafDataChangedEvent event) {
+        load();
     }
     
 }
