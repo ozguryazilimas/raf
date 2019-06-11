af506fe602d671d7a9260a864f25d8909a04781d
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2019-06-11 08:45:17 +0300

    refs #33519 previewlar için hata kontrolü yapıldı

--- raf-converter/src/main/java/com/ozguryazilim/raf/preview/ImagePreviewSequencer.java
+++ raf-converter/src/main/java/com/ozguryazilim/raf/preview/ImagePreviewSequencer.java
@@ -14,54 +14,62 @@ import org.imgscalr.Scalr;
 import org.modeshape.common.util.CheckArg;
 import org.modeshape.jcr.api.nodetype.NodeTypeManager;
 import org.modeshape.jcr.api.sequencer.Sequencer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Raf'a yüklenen görsellerden preview amacıyla scale edilmiş sürümler üretir.
- * 
+ *
  * @author Hakan Uygun
  */
-public class ImagePreviewSequencer extends Sequencer{
+public class ImagePreviewSequencer extends Sequencer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ImagePreviewSequencer.class);
 
     @Override
     public void initialize(NamespaceRegistry registry, NodeTypeManager nodeTypeManager) throws RepositoryException, IOException {
         super.registerNodeTypes("/preview.cnd", nodeTypeManager, true);
     }
-    
+
     @Override
     public boolean execute(Property inputProperty, Node outputNode, Context context) throws Exception {
-        
-        Binary binaryValue = inputProperty.getBinary();
-        CheckArg.isNotNull(binaryValue, "binary");
-        //Node sequencedNode = getPdfMetadataNode(outputNode);
-    
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        
-        BufferedImage src = ImageIO.read(binaryValue.getStream());
-        BufferedImage scaledImg = Scalr.resize(src, Scalr.Method.BALANCED, 480, 320, Scalr.OP_ANTIALIAS);
-        ImageIO.write(scaledImg, "png", os);
-        
-        src.flush();
-        scaledImg.flush();
-        
-        ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
-        
-        Node previewNode = getPreviewNode(outputNode);
-        
-        Binary preview = outputNode.getSession().getValueFactory().createBinary(is);
-        previewNode.setProperty("jcr:mimeType", "image/png");
-        previewNode.setProperty("jcr:data", preview);
-        
+
+        try {
+            Binary binaryValue = inputProperty.getBinary();
+            CheckArg.isNotNull(binaryValue, "binary");
+            //Node sequencedNode = getPdfMetadataNode(outputNode);
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            BufferedImage src = ImageIO.read(binaryValue.getStream());
+            BufferedImage scaledImg = Scalr.resize(src, Scalr.Method.BALANCED, 480, 320, Scalr.OP_ANTIALIAS);
+            ImageIO.write(scaledImg, "png", os);
+
+            src.flush();
+            scaledImg.flush();
+
+            ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
+
+            Node previewNode = getPreviewNode(outputNode);
+
+            Binary preview = outputNode.getSession().getValueFactory().createBinary(is);
+            previewNode.setProperty("jcr:mimeType", "image/png");
+            previewNode.setProperty("jcr:data", preview);
+        } catch (Exception e) {
+            LOG.warn("Preview cannot generated", e);
+            return false;
+        }
+
         //Yapılan değişiklikler kayıt edilsin.
         return true;
     }
-    
-    
-    private Node getPreviewNode( Node outputNode ) throws RepositoryException{
-        if( outputNode.isNew() ){
+
+    private Node getPreviewNode(Node outputNode) throws RepositoryException {
+        if (outputNode.isNew()) {
             outputNode.setPrimaryType("raf:preview");
             return outputNode;
         }
-        
+
         return outputNode.addNode("raf:preview", "raf:preview");
     }
 }
--- raf-converter/src/main/java/com/ozguryazilim/raf/preview/OfficePreviewSequencer.java
+++ raf-converter/src/main/java/com/ozguryazilim/raf/preview/OfficePreviewSequencer.java
@@ -12,56 +12,63 @@ import javax.jcr.RepositoryException;
 import org.modeshape.common.util.CheckArg;
 import org.modeshape.jcr.api.nodetype.NodeTypeManager;
 import org.modeshape.jcr.api.sequencer.Sequencer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author oyas
  */
-public class OfficePreviewSequencer extends Sequencer{
+public class OfficePreviewSequencer extends Sequencer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OfficePreviewSequencer.class);
 
     @Override
     public void initialize(NamespaceRegistry registry, NodeTypeManager nodeTypeManager) throws RepositoryException, IOException {
         super.registerNodeTypes("/preview.cnd", nodeTypeManager, true);
     }
 
-    
-    
     @Override
     public boolean execute(Property inputProperty, Node outputNode, Context context) throws Exception {
-        
-        Binary binaryValue = inputProperty.getBinary();
-        CheckArg.isNotNull(binaryValue, "binary");
-        //Node sequencedNode = getPdfMetadataNode(outputNode);
-        
-        PDFPreviewConverter converter = new PDFPreviewConverter();
-        
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        
-        String mimeType = inputProperty.getParent().getProperty("jcr:mimeType").getString();
-        
-        converter.convert(binaryValue.getStream(), mimeType, os);
-        
-        Node previewNode = getPreviewNode(outputNode);
-        
-        
-        ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
-        
-        Binary previewPfd = outputNode.getSession().getValueFactory().createBinary(is);
-        previewNode.setProperty("jcr:mimeType", "application/pdf");
-        previewNode.setProperty("jcr:data", previewPfd);
-        
+
+        try {
+            Binary binaryValue = inputProperty.getBinary();
+            CheckArg.isNotNull(binaryValue, "binary");
+            //Node sequencedNode = getPdfMetadataNode(outputNode);
+
+            PDFPreviewConverter converter = new PDFPreviewConverter();
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            String mimeType = inputProperty.getParent().getProperty("jcr:mimeType").getString();
+
+            converter.convert(binaryValue.getStream(), mimeType, os);
+
+            Node previewNode = getPreviewNode(outputNode);
+
+            ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
+
+            Binary previewPfd = outputNode.getSession().getValueFactory().createBinary(is);
+            previewNode.setProperty("jcr:mimeType", "application/pdf");
+            previewNode.setProperty("jcr:data", previewPfd);
+
+        } catch (Exception e) {
+            LOG.warn("Preview cannot generated", e);
+            return false;
+        }
+
         //Yapılan değişiklikler kayıt edilsin.
         return true;
-        
+
     }
-    
-    private Node getPreviewNode( Node outputNode ) throws RepositoryException{
-        if( outputNode.isNew() ){
+
+    private Node getPreviewNode(Node outputNode) throws RepositoryException {
+        if (outputNode.isNew()) {
             outputNode.setPrimaryType("raf:preview");
             return outputNode;
         }
-        
+
         return outputNode.addNode("raf:preview", "raf:preview");
     }
-    
+
 }
