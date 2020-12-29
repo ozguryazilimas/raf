7e5c5bded457bca94e06da1d8a0d8a505aa931b2
Hakan Uygun <hakan.uygun@ozguryazilim.com.tr>
2020-12-29 11:31:36 +0300

    refs #44275 yetki kontorlleri düzeltildi

--- raf-ui/src/main/java/com/ozguryazilim/raf/action/PasteAction.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/action/PasteAction.java
@@ -56,7 +56,7 @@ public class PasteAction extends AbstractAction {
             if (getContext().getSelectedObject() != null && !Strings.isNullOrEmpty(identity.getLoginName()) && !Strings.isNullOrEmpty(getContext().getSelectedObject().getPath()) && rafPathMemberService.hasMemberInPath(identity.getLoginName(), getContext().getSelectedObject().getPath())) {
                 permission = rafPathMemberService.hasWriteRole(identity.getLoginName(), getContext().getSelectedObject().getPath());
             } else {
-                permission = getContext().getSelectedRaf().getId() > 0 && memberService.hasWriteRole(identity.getLoginName(), getContext().getSelectedRaf());
+                permission = memberService.hasWriteRole(identity.getLoginName(), getContext().getSelectedRaf());
             }
             return permission && super.applicable(forCollection);
         } catch (RafException ex) {
