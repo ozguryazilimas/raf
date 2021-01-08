85d3cdecc9ec9a4760868553f57f92f135959682
Deniz Bahadır GÜR <dbahadir@gmail.com>
2021-01-08 04:56:24 +0300

    added new encoder for raf name and replaced all dot characters

---
+++ raf-core/src/main/java/com/ozguryazilim/raf/encoder/RafNameEncoder.java
@@ -0,0 +1,89 @@
+package com.ozguryazilim.raf.encoder;
+
+import java.util.regex.Pattern;
+
+
+/**
+ * Raf name için encode decode işlemi yapar.
+ *
+ * Aslında decode yapmayacağız. Sadece encode yapacağız.
+ *
+ * Türkçe karakterler ingilizce karşılığına, boşluklar ise alt çicgiye dönüşecek
+ *
+ * @author Hakan Uygun
+ */
+public class RafNameEncoder implements RafEncoder {
+
+    public String encode(String text) {
+
+        StringBuilder sb = new StringBuilder();
+        //toLowerCase(new Locale("tr")) hepsini küçük harfer çevirmek için eklenebilir
+        if (text.split("\\.").length > 2) {
+            String extension = text.substring(text.lastIndexOf("."));
+            text = text.replaceAll(Pattern.quote(extension), "").replaceAll("\\.", "_").concat(extension);
+        }
+        char[] source = text.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            switch (source[i]) {
+                case 'ğ':
+                    sb.append('g');
+                    break;
+                case 'ı':
+                    sb.append('i');
+                    break;
+                case 'ü':
+                    sb.append('u');
+                    break;
+                case 'ş':
+                    sb.append('s');
+                    break;
+                case 'ç':
+                    sb.append('c');
+                    break;
+                case 'ö':
+                    sb.append('o');
+                    break;
+                case 'Ğ':
+                    sb.append('G');
+                    break;
+                case 'İ':
+                    sb.append('I');
+                    break;
+                case 'Ü':
+                    sb.append('U');
+                    break;
+                case 'Ş':
+                    sb.append('S');
+                    break;
+                case 'Ç':
+                    sb.append('C');
+                    break;
+                case 'Ö':
+                    sb.append('O');
+                    break;
+                case '[':
+                    sb.append('(');
+                    break;
+                case ']':
+                    sb.append(')');
+                    break;
+                case '.':
+                case ' ':
+                case ':':
+                case '\'':
+                    sb.append('_');
+                    break;
+                default:
+                    sb.append(source[i]);
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+    public String decode(String encodedText) {
+        //Geriye dönüş için özel bir şey yapmıyoruz.
+        return encodedText;
+    }
+}
