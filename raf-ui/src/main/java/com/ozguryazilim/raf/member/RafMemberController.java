947d3d9c6e4faecb9dca40a82dd070cdcea5681d
Deniz Bahadır Gür <deniz.gur@ozguryazilim.com.tr>
2021-11-09 13:51:39 +0300

    Revert "Merge branch 'fix/52809/raf_member_user_name_filter_clean_code' into 'master'"
    This reverts merge request !112

--- raf-ui/src/main/java/com/ozguryazilim/raf/member/RafMemberController.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/member/RafMemberController.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
@@ -74,7 +75,7 @@ public class RafMemberController implements Serializable {
     private String filter = "";
     private List<RafMember> filteredMembers;
     private RafMember selectedMember;
-
+    
     public void init() {
 
         filteredMembers = null;
@@ -127,22 +128,17 @@ public class RafMemberController implements Serializable {
         this.rafDefinition = rafDefinition;
     }
 
-    private boolean filterMembers(RafMember m) {
-        if (m.getMemberType().equals(RafMemberType.USER)) {
-            return userLookup.getUserName(m.getMemberName()).contains(filter);
-        } else {
-            return m.getMemberName().contains(filter) || getGroupUserNames(m.getMemberName()).stream().filter(gm -> gm.contains(filter)).findAny().isPresent();
-        }
-    }
-
     public List<RafMember> getMembers() {
-
-        if (filteredMembers == null) {
+        
+        if( filteredMembers == null ){
             try {
-                filteredMembers = memberService.getMembers(rafDefinition).stream()
-                        .filter(m -> filterMembers(m))
+                filteredMembers= memberService.getMembers(rafDefinition).stream()
+                        .filter( m->  
+                                Strings.isNullOrEmpty(m.getMemberName()) ? false : 
+                                    m.getMemberType().equals(RafMemberType.USER) ? 
+                                        userLookup.getUserName(m.getMemberName()).contains(filter) : m.getMemberName().contains( filter))
                         .collect(Collectors.toList());
-
+                
                 return filteredMembers;
             } catch (RafException ex) {
                 //FIXME: i18n
@@ -152,7 +148,7 @@ public class RafMemberController implements Serializable {
 
             return Collections.emptyList();
         }
-
+        
         return filteredMembers;
     }
 
@@ -254,10 +250,6 @@ public class RafMemberController implements Serializable {
         return memberService.getGroupUsers(userGroupName);
     }
 
-    private List<String> getGroupUserNames(String userGroupName) {
-        return memberService.getGroupUsers(userGroupName).stream().map(m -> userLookup.getUserName(m)).collect(Collectors.toList());
-    }
-
     public void deleteMember(RafMember member) {
         try {
             memberService.removeMember(member);
@@ -268,10 +260,11 @@ public class RafMemberController implements Serializable {
         }
     }
 
-    public void search() {
+    
+    public void search(){
         filteredMembers = null;
     }
-
+    
     public String getFilter() {
         return filter;
     }
@@ -287,28 +280,29 @@ public class RafMemberController implements Serializable {
     public void setSelectedMember(RafMember selectedMember) {
         this.selectedMember = selectedMember;
     }
+ 
 
-    public void editMember(RafMember m) {
+    public void editMember( RafMember m ){
         selectedMember = m;
     }
-
-    public void closeDialog() {
+    
+    public void closeDialog(){
         //Normal bir kapanış olduğuna göre ya save işlemi yapılacak demek.
-
-        if (selectedMember != null) {
-
+        
+        if( selectedMember != null){
+            
             try {
                 memberService.changeMemberRole(selectedMember.getRaf(), selectedMember.getMemberName(), selectedMember.getRole());
             } catch (RafException ex) {
                 LOG.error("Role Cannot Change", ex);
             }
-
+            
             search();
         }
-
+        
     }
-
-    public void cancelDialog() {
+    
+    public void cancelDialog(){
         //Aslında yapacak bir şey yok.
         selectedMember = null;
     }
