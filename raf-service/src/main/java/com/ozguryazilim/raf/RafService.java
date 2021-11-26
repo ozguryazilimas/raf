886155a3846a3d1a64bbe3d267ce6b0cec870f11
evren.cengiz <evren.cengiz@ozguryazilim.com.tr>
2021-11-26 17:38:24 +0300

    feat: add user rafs to user view page
    Refs: #52823

--- raf-service/src/main/java/com/ozguryazilim/raf/RafService.java
+++ raf-service/src/main/java/com/ozguryazilim/raf/RafService.java
@@ -31,6 +31,8 @@ import javax.inject.Inject;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import org.apache.deltaspike.core.api.config.ConfigResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Raf hizmetleri için temel Service sınıfı.
@@ -41,6 +43,8 @@ import org.apache.deltaspike.core.api.config.ConfigResolver;
  */
 @ApplicationScoped
 public class RafService implements Serializable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RafModeshapeRepository.class);
 
     @Inject
     private RafModeshapeRepository rafRepository;
