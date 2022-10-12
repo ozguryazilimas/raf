33e7d216ad897820c28ad84e00fa2143289ef1d0
emrygun <emre6271@gmail.com>
2022-10-12 13:40:59 +0300

    refs #66728 added external fill sort query

--- raf-ui/src/main/java/com/ozguryazilim/raf/search/SearchResultDataModel.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/search/SearchResultDataModel.java
@@ -74,6 +74,7 @@ public class SearchResultDataModel extends LazyDataModel<RafObject> {
                     LOG.error("One or more files binary data could not found while searching. Retrying without data search.", ex);
 
                     searchModel.setSearchInFileDataAvailable(false);
+                    fillSortQueries(searchPanels, extendedQuery, extendedSortQuery);
                     fillSearchModelForModeshapeStrategy(pageSize, first, sortField, sortOrder, extendedQuery, extendedSortQuery);
                     searchModel.setSearchInFileDataAvailable(true);
 
