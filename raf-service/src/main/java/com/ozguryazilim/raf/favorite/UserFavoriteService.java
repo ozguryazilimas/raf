9460c1e669696c5b08957ad877fab775906c2715
emrygun <emre6271@gmail.com>
2023-03-14 20:39:15 +0300

    refs #72956 removed unnecessary imports

--- raf-service/src/main/java/com/ozguryazilim/raf/favorite/UserFavoriteService.java
+++ raf-service/src/main/java/com/ozguryazilim/raf/favorite/UserFavoriteService.java
@@ -1,7 +1,6 @@
 package com.ozguryazilim.raf.favorite;
 
 import com.ozguryazilim.raf.entities.UserFavorite;
-import com.ozguryazilim.raf.models.RafObject;
 import org.apache.deltaspike.jpa.api.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +11,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @ApplicationScoped
