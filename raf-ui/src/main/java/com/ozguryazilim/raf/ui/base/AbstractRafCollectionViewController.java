8923ec6c7bb68f33cbcb5890df8f19eae1609f3a
emrygun <emre6271@gmail.com>
2023-05-10 11:46:45 +0300

    refs #73475 rafCollection pagingLimit impl

--- raf-ui/src/main/java/com/ozguryazilim/raf/ui/base/AbstractRafCollectionViewController.java
+++ raf-ui/src/main/java/com/ozguryazilim/raf/ui/base/AbstractRafCollectionViewController.java
@@ -157,4 +157,5 @@ public abstract class AbstractRafCollectionViewController implements RafCollecti
         rafController.nextPage();
         folderDataChangeEvent.fire(new RafFolderDataChangeEvent());
     }
+
 }
